4a4460bb71483436eca8c3f61906ceb186afef20
AurimasJa <aurimasa50@gmail.com>
2022-10-29 16:41:56 +0300

    GameMatches CRUD + Service

---
+++ BattleShipV3/Server/Controllers/GameMatchesController.cs
@@ -0,0 +1,106 @@
+﻿using BattleShipV3.Data;
+using BattleShipV3.Models;
+using BattleShipV3.Server.Repositories;
+using BattleShipV3.Shared.Data.Commands.Listing.Create;
+using BattleShipV3.Shared.Data.Commands.Listing.Get;
+using BattleShipV3.Shared.Data.Commands.Listing.Update;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BattleShipV3.Server.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class GameMatchesController : ControllerBase
+{
+
+    private readonly IListingsRepository _listingsRepository;
+    private readonly IUsersRepository _usersRepository;
+    private readonly IGameMatchesRepository _gameMatchesRepository;
+
+    public GameMatchesController(IListingsRepository listingsRepository, IUsersRepository usersRepository, IGameMatchesRepository gameMatchesRepository)
+    {
+        _listingsRepository = listingsRepository;
+        _usersRepository = usersRepository;
+        _gameMatchesRepository = gameMatchesRepository;
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<GetGameMatchCommand>> GetGameMatchAsync(int? id)
+    {
+        var gameMatch = await _gameMatchesRepository.GetGameMatchAsync(id);
+        if (gameMatch == null)
+            return NotFound($"Game Match does not exist");
+
+        return new GetGameMatchCommand(gameMatch.Id, gameMatch.CreationDate, gameMatch.GameState, gameMatch.Listing, gameMatch.User);
+    }
+    [HttpGet]
+    public async Task<IEnumerable<GetGameMatchCommand>> GetGameMatchesAsync()
+    {
+        var gameMatches = await _gameMatchesRepository.GetAllGameMatchesAsync();
+        return gameMatches.Select(x => new GetGameMatchCommand(x.Id, x.CreationDate, x.GameState, x.Listing, x.User));
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<GetGameMatchCommand>> CreateGameMatchAsync(CreateGameMatchCommand createGameMatchCommand)
+    {
+        //var listings = await _usersRepository.get();
+        if (createGameMatchCommand == null)
+        {
+            return BadRequest("Error");
+        }
+        //gauti listing id is prasidejusio matcho
+        var listing = await _listingsRepository.GetListingAsync(createGameMatchCommand.Listing.Id);
+
+        var gameMatch = new GameMatch
+        {
+            GameState = Enums.GameState.ACTIVE,
+            CreationDate = DateTime.UtcNow,
+            Listing = listing,
+            User = listing.PlayerOne
+        };
+
+        await _gameMatchesRepository.CreateGameMatchAsync(gameMatch);
+        return Created("", new GetGameMatchCommand(gameMatch.Id, gameMatch.CreationDate, gameMatch.GameState, gameMatch.Listing, gameMatch.User));
+    }
+
+    [HttpPut]
+    [Route("{gameMatchId}")]
+    public async Task<ActionResult<GetListingCommand>> UpdateListingAsync(int gameMatchId, UpdateGameMatchCommand updateGameMatchCommand)
+    {
+        
+        var gameMatch = await _gameMatchesRepository.GetGameMatchAsync(gameMatchId);
+        var listing = await _listingsRepository.GetListingAsync(gameMatch.Listing.Id);
+
+        // 404 UpdateListingCommand(string? Name, double? EloFrom, double? EloTo);
+        if (gameMatch == null)
+            return NotFound($"No game match with id of {gameMatchId}");
+
+        //geras
+        //var user = await _usersRepository.GetUserAsync(updateGameMatchCommand.UserWinner.Id);
+        var user = await _usersRepository.GetUserAsync(listing.PlayerTwo.Id);
+
+
+        gameMatch.GameState = updateGameMatchCommand.GameState is 0 ? gameMatch.GameState : updateGameMatchCommand.GameState; // jei 0 tai palikti 0
+        gameMatch.User = updateGameMatchCommand.UserWinner is null ? gameMatch.User : user; // sutvarkyti kuris useris winneris irgi is frontend
+
+        await _gameMatchesRepository.UpdateGameMatchAsync(gameMatch);
+
+        return Ok(new GetGameMatchCommand(gameMatch.Id, gameMatch.CreationDate, gameMatch.GameState, gameMatch.Listing, gameMatch.User));
+    }
+
+    [HttpDelete("{gameMatchId}")]
+    public async Task<ActionResult> Remove(int gameMatchId)
+    {
+        var gameMatch = await _gameMatchesRepository.GetGameMatchAsync(gameMatchId);
+
+        // 404
+        if (gameMatch == null)
+            return NotFound($"No game match with id of {gameMatchId}"); ;
+
+        await _gameMatchesRepository.DeleteGameMatchAsync(gameMatch);
+
+
+        // 204
+        return NoContent();
+    }
+}
---
+++ BattleShipV3/Shared/Data/Commands/GameMatch/Create/CreateGameMatchCommand.cs
@@ -0,0 +1,5 @@
+﻿
+using BattleShipV3.Models;
+using static BattleShipV3.Data.Enums;
+
+public record CreateGameMatchCommand(Listing Listing);
