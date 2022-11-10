24d030255d65ea1e5e8349503ebe46a04a6ff1df
Laurynas <laurynas.cekavicius@ktu.edu>
2022-11-10 22:18:26 +0200

    Update

---
+++ BattleShipV3/Shared/Data/Interfacess/IActualMap.cs
@@ -0,0 +1,26 @@
+﻿using BattleShipV3.Client.DesignPatterns.Prototype;
+using BattleShipV3.Data.Models;
+using BattleShipV3.Shared.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleShipV3.Shared.Data.Interfacess
+{
+    public interface IActualMap
+    {
+        public List<PlaySquare> playSquaresDeep { get; set; }
+        public List<PlaySquare> playSquaresShallow { get; set; }
+        //public Square[,] playField { get; set; }
+        public List<Square[]> playField { get; set; }
+        public PlaySquare oneplaySquare { get; set; }
+        bool LandMissile(int x, int y);
+        bool IsShipPlaced(Ship ship, int x, int y);
+        Square GetSquare(int x, int y);
+        bool IsAllowedToPlace(Ship ship, int x, int y);
+        void AssignShip(Ship ship, int x, int y);
+        void PrototypeCopies(int x, int y, string actionName);
+    }
+}
---
+++ BattleShipV3/Shared/Data/Models/Square.cs
@@ -0,0 +1,40 @@
+﻿using BattleShipV3.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static BattleShipV3.Data.Enums;
+
+namespace BattleShipV3.Shared.Data.Models
+{
+    public class Square
+    {
+        public SquareType squareType { get; set; }
+        public Ship? ship { get; set; }
+
+        public Square(SquareType squareType, Ship ship)
+        {
+            this.squareType = squareType;
+            this.ship = ship;
+        }
+
+        public void AssignShip(Ship ship)
+        {
+            this.ship = ship;
+            this.squareType = SquareType.SHIP;
+        }
+
+        public bool LandMissile()
+        {
+            if (squareType == SquareType.SHIP)
+            {
+                squareType = SquareType.DESTROYED;
+                return true;
+            }
+
+            squareType = SquareType.MISSED;
+            return false;
+        }
+    }
+}
