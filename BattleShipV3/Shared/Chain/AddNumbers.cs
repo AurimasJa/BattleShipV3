a5e1ee4b4efe6a7201c38f494c03233bc49c0f3e
AurimasJa <aurimasa50@gmail.com>
2022-12-16 00:33:49 +0200

    chain +-

---
+++ BattleShipV3/Shared/Chain/AddNumbers.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleShipV3.Shared.Chain
+{
+    public class AddNumbers : AbstractHandler
+    {
+        double number { get; set; }
+
+        public AddNumbers(double number)
+        {
+            this.number = number;
+        }
+
+        public override object Handle(object request)
+        {
+            if ((request as string) == "20")
+            {
+                return number + 10;
+            }
+            else
+            {
+                return base.Handle(request);
+            }
+        }
+
+    }
+}
---
+++ BattleShipV3/Shared/Chain/MultNumbers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleShipV3.Shared.Chain
+{
+    public class MultNumbers : AbstractHandler
+    {
+        double number;
+
+        public MultNumbers(double number)
+        {
+            this.number = number;
+        }
+        public override object Handle(object request)
+        {
+            if (request.ToString() == "10")
+            {
+                return 10 * number;
+            }
+            else
+            {
+                return base.Handle(request);
+            }
+        }
+
+    }
+}
