21b6601f89cb5ca63aaf0c72dad2d1d2f7802cfe
Laurynas <laurynas.cekavicius@ktu.edu>
2022-12-22 20:13:47 +0200

    useless

---
+++ BattleShipV3/Client/DesignPatterns/Lab2/State/LoginState.cs
@@ -0,0 +1,10 @@
+﻿namespace BattleShipV3.Client.DesignPatterns.Lab2.State
+{
+    public class LoginState : State
+    {
+        public override void SetRenderFragment()
+        {
+            this._context.TransitionTo(new LoginState());
+        }
+    }
+}
---
+++ BattleShipV3/Client/DesignPatterns/Lab2/State/State.cs
@@ -0,0 +1,16 @@
+﻿using BattleShipV3.Client.Pages;
+
+namespace BattleShipV3.Client.DesignPatterns.Lab2.State
+{
+    public abstract class State
+    {
+        protected IndexView _context;
+
+        public void SetContext(IndexView context)
+        {
+            this._context = context;
+        }
+
+        public abstract void SetRenderFragment();
+    }
+}
